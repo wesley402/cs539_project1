73a55410ba1265a9cdc2df1800f443d582a00d3d
Nick Romano <nickpromano@gmail.com>
2018-03-27 00:05:25 -0400

    updated flights view in manager ui

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from accounts.models import Profile
 
 from django.contrib.auth.models import User
+from django.db import connection
 
 
 # Create your views here.
@@ -17,7 +18,28 @@ def customer(request):
     return render(request, "dashboard/customers.html",context=context)
 
 def flight(request):
-    return render(request, "dashboard/flights.html")
+    with connection.cursor() as cursor:
+        cursor.callproc('getAllFlights')
+        columns = [col[0] for col in cursor.description]
+        result = [dict(zip(columns, row)) for row in cursor.fetchall()]
+        context = {
+            "flights":result[:100]
+        }
+    return render(request, "dashboard/flights.html",context=context)
 
 def reservation(request):
     return render(request, "dashboard/reservations.html")
+ 
+# def my_custom_sql(self):
+#     cursor = connection.cursor()
+#     cursor.execute("SELECT * FROM accounts_account where id=%s",[self.id])
+#     row = cursor.fetchall()
+#     return row
+
+# def dictfetchall(cursor):
+#     "Return all rows from a cursor as a dict"
+#     columns = [col[0] for col in cursor.description]
+#     return [
+#         dict(zip(columns, row))
+#         for row in cursor.fetchall()
+#     ]
